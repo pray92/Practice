54172a96cd117e389f9aa8e5a0d9ea7df5647874
jisu Choi <pray92@users.noreply.github.com>
2021-05-04 23:18:33 +0900

    프로그래머스 - 짝지어 제거하기, 효율성 이슈 해결

--- C_Plus_Plus/Programmers/RemoveWithParing.cpp
+++ C_Plus_Plus/Programmers/RemoveWithParing.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include<string>
+#include <string>
+#include <stack>
+
 using namespace std;
 
+#if PENDING // 효율성 실패
 int solution(string s)
 {
     int answer = 0;
@@ -28,11 +31,37 @@ int solution(string s)
 
     return answer;
 }
+#endif
+
+int solution(string s)
+{
+    int answer = 0;
+    // 홀수는 무조건 하나가 남습니다.
+    if(s.empty() || s.size() % 2 != 0)
+        return answer;
+
+    stack<char> stk;
+    for(size_t nIndex = 0; nIndex < s.size() ; ++nIndex)
+    {
+        if(stk.empty() || stk.top() != s[nIndex])
+            stk.push(s[nIndex]);
+        else
+            stk.pop();
+        
+    }
+
+    if(stk.empty())
+        ++answer;
+
+    return answer;
+}
 
 int main()
 {
     cout << solution("baabaa") << endl;
     cout << solution("cdcd") << endl;
+    cout << solution("aabb") << endl;
+    cout << solution("aabbccddbbaadd") << endl;
 
     return 0;
 }
--- C_Plus_Plus/main.cpp
+++ C_Plus_Plus/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    vector<string> strings;
-    strings.push_back("12342");
-    strings.push_back("12");
-    strings.push_back("1");
-    strings.push_back("2");
-    strings.push_back("234");
-    strings.push_back("23466");
-    sort(strings.begin(), strings.end());
-    for(auto str : strings)
-        cout << str << endl;
+    cout << "Hello World" << endl;
 
     return 0;
 }
