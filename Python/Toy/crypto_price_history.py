9875ffe5f9d566f0c89ab1ce4227d639e13d1e65
jisu Choi <pray92@users.noreply.github.com>
2021-05-23 18:59:41 +0900

    Streamlit 라이브러리를 활용한 웹앱 - By 빵형

---
+++ Python/Toy/crypto_price_history.py
@@ -0,0 +1,30 @@
+
+'''
+가상화폐를 조회하는 웹앱 프로젝트
+'''
+
+import streamlit as st
+from cryptocmd import CmcScraper
+from streamlit.elements.checkbox import CheckboxMixin
+import plotly.express as px
+from datetime import datetime
+
+st.write('# Cryptocurrency Web App')
+
+st.sidebar.header('Menu')
+
+name = st.sidebar.selectbox('Name', ['BTC', 'ETH', 'USDT'])
+
+start_date = st.sidebar.date_input('Start date', datetime(2021, 1, 1))
+end_date = st.sidebar.date_input('Start date', datetime(2021, 1, 7))
+
+# https://coinmarketcap.com
+scraper = CmcScraper(name, start_date.strftime('%d-%m-%Y'), end_date.strftime('%d-%m-%Y'))
+
+df = scraper.get_dataframe()
+
+fig_close = px.line(df, x='Date', y=['Open', 'High', 'Low', 'Close'], title='Price')
+fig_volume = px.line(df, x='Date', y=['Volume'], title='Volume')
+
+st.plotly_chart(fig_close)
+st.plotly_chart(fig_volume)
