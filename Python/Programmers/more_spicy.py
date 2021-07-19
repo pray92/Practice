81b8bf99c8fbcc3842d6090c15400da162f15291
jisu Choi <pray92@users.noreply.github.com>
2021-07-19 23:15:26 +0900

    프로그래머스 - 더 맵게 2차(풀이 실패) - python

--- Python/Programmers/more_spicy.py
+++ Python/Programmers/more_spicy.py
@@ -1,13 +1,34 @@
+import heapq
+from collections import deque
 
 def solution(scoville, K):
-	scoville = list(filter(lambda x: x < K, sorted(scoville)))
+	filtered_scoville = list(filter(lambda x: x < K, sorted(scoville)))
+	heapq.heapify(filtered_scoville)
+
+	mixable = len(scoville) != len(filtered_scoville)
 
 	answer = 0
 
+	while len(filtered_scoville) >= 2:
+		food1 =	heapq.heappop(filtered_scoville)
+		food2 =	heapq.heappop(filtered_scoville)
+
+		mixed = food1 + (food2) * 2
+		answer += 1
+
+		heapq.heappush(filtered_scoville, mixed)
 	
+	last_food = filtered_scoville[0]
+	if last_food >= K:
+		return answer
+	
+	if mixable:
+		return answer + 1
 
 	return -1
 
 
 if __name__ == '__main__':
 	print(solution([1, 2, 3, 9, 10, 12], 7))
+	print(solution([100, 1], 2))
+	print(solution([1] * 1000000, 2))
