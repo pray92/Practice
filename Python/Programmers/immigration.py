8bc9d4194f8ad42ad3f9c5905bcbe10deb018115
jisu Choi <pray92@users.noreply.github.com>
2021-07-21 23:08:43 +0900

    프로그래머스 - 입국심사(풀이실패 2차) - python

--- Python/Programmers/immigration.py
+++ Python/Programmers/immigration.py
@@ -1,17 +1,40 @@
-from queue import PriorityQueue
+
+def get_process_count(candidate, times):
+	ret = 0
+	for time in times:
+		ret += candidate // time
+	return ret
 
 def solution(n, times):
+	max_time = min(times) * n
+	candidates = []
+	for i in range(1, n + 1):
+		for time in times:
+			if i * time > max_time:
+				break
+			candidates.append(i * time)
+	candidates.sort()
+
 	answer = 0
 
-	q = PriorityQueue()
-	goes_on = 1
-	while q.qsize() < n:
-		for time in times:
-			q.put(time * goes_on)
-		goes_on += 1
+	mid = len(candidates) // 2
+	left, right = mid, len(candidates)
+	while left <= right:
+		processed = get_process_count(candidates[mid], times)
+		if processed > n:
+			right = mid
+		elif processed < n:
+			left = mid
+		else:
+			answer = candidates[mid]
+			break
+		mid = (left + right) // 2
 
 
-	return q.get()
+		
+
+
+	return answer
 
 if __name__ == '__main__':
 	print(solution(6, [7, 10]))
