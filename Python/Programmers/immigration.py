8c58e6ccb4148c27908e61dabc54a9ab8c07bae9
jisu Choi <pray92@users.noreply.github.com>
2021-07-22 20:45:10 +0900

    프로그래머스 - 입국심사(풀이완료)

--- Python/Programmers/immigration.py
+++ Python/Programmers/immigration.py
@@ -1,40 +1,21 @@
 
-def get_process_count(candidate, times):
-	ret = 0
-	for time in times:
-		ret += candidate // time
-	return ret
-
 def solution(n, times):
-	max_time = min(times) * n
-	candidates = []
-	for i in range(1, n + 1):
-		for time in times:
-			if i * time > max_time:
-				break
-			candidates.append(i * time)
-	candidates.sort()
-
 	answer = 0
+	
+	low, mid, high = 1, 0, max(times) * n
+	while low < high:
+		mid = (low + high) // 2
 
-	mid = len(candidates) // 2
-	left, right = mid, len(candidates)
-	while left <= right:
-		processed = get_process_count(candidates[mid], times)
-		if processed > n:
-			right = mid
-		elif processed < n:
-			left = mid
-		else:
-			answer = candidates[mid]
-			break
-		mid = (left + right) // 2
-
-
-		
-
-
+		checked = 0
+		for time in times:
+			checked += mid // time
+			
+		if checked < n:
+			low = mid + 1
+		elif checked >= n:
+			high = mid
+	answer = low
 	return answer
 
 if __name__ == '__main__':
-	print(solution(6, [7, 10]))
+	print(solution(7, [3, 3, 3, 2, 10]))
