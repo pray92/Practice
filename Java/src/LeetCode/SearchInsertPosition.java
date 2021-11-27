32c67cf018e2646ffa9d19cc90335bdb2337d77e
Jisu Choi <redgem92@gmail.com>
2021-11-27 16:38:03 +0900

    Leet Code - Search Insert Position

---
+++ Java/src/LeetCode/SearchInsertPosition.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int mid = 0;
+        while(left <= right){
+            mid = (left + right ) >>> 1;
+            if(nums[mid] == target){
+            } else if(nums[mid] < target){
+                left = mid + 1;
+            } else if(nums[mid] > target){
+                right = mid - 1;
+            } else{
+                break;
+            }
+        }
+
+        if(nums[mid] < target)
+            ++mid;
+
+        return mid;
+    }
+
+    public static void main(String[] args){
+        new SearchInsertPosition().searchInsert(new int[]{1,3,5,6}, 5);
+    }
+}
