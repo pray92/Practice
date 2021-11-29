9e683bb430170a7fc94526e4006f9ab9eee0df3f
jisu Choi <pray92@users.noreply.github.com>
2021-11-29 21:22:24 +0900

    FIX : Leet Code - Divide Two Integers

--- Java/src/LeetCode/DivideTwoIntegers.java
+++ Java/src/LeetCode/DivideTwoIntegers.java
@@ -2,32 +2,45 @@ package LeetCode;
 
 public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
+        long dividend2 = dividend;
         boolean isDividendMinus = dividend < 0;
-        if(isDividendMinus){
-            if(dividend == Integer.MIN_VALUE){
-                dividend += 1;
-            }
-            dividend = -dividend;
-        }
+        if(isDividendMinus)
+            dividend2 = -dividend2;
+
+        long divisor2 = divisor;
         boolean isDivisorMinus = divisor < 0;
-        if(isDivisorMinus){
-            if(divisor == Integer.MIN_VALUE){
-                divisor += 1;
+        if(isDivisorMinus)
+            divisor2 = -divisor2;
+
+        long ret = 0;
+        while(divisor2 <= dividend2){
+            int mask;
+            for(mask = 0; mask <= 30; ++mask){
+                if((divisor2 << (mask + 1)) > dividend2)
+                    break;
             }
-            divisor = -divisor;
+
+            ret += (1L << mask);
+            dividend2 -= (divisor2 << mask);
         }
 
-        int ret = 0;
+        if((isDividendMinus && !isDivisorMinus) || (!isDividendMinus && isDivisorMinus)){
+            if(-ret < Integer.MIN_VALUE)
+                ret = (Integer.MIN_VALUE + 1);
+            return (int)-ret;
+        }
 
-        if((isDividendMinus && !isDivisorMinus)
-        ||(!isDividendMinus && isDivisorMinus))
-            return -ret;
+        if(ret > Integer.MAX_VALUE)
+            ret = Integer.MAX_VALUE;
 
-        return ret;
+        return (int)ret;
     }
 
     public static void main(String[] args){
         new DivideTwoIntegers().divide(-2147483648, -1);
+        new DivideTwoIntegers().divide(7, -3);
+
+        //System.out.println((1L << 31) + " " + Integer.MAX_VALUE);
     }
 
 }
