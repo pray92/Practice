b3bf1e2439eaa077c17fcaf86177e5784457188a
jisu Choi <pray92@users.noreply.github.com>
2021-12-08 21:06:54 +0900

    Leet Code - FAIL : Longest Vaild Parentheses

--- Java/src/LeetCode/LongestVaildParentheses.java
+++ Java/src/LeetCode/LongestVaildParentheses.java
@@ -4,28 +4,43 @@ import java.util.Stack;
 
 public class LongestVaildParentheses {
     public int longestValidParentheses(String s) {
-        int ret = 0, cnt = 0;
-        Stack<Character> stk = new Stack<>();
-        for(int i = 0; i < s.length(); ++i){
-            switch(s.charAt(i)){
+        for(int i = 0; i <= s.length() - 2; ++i){
+            for(int j = 0; j <= i; ++j){
+                if(isValid(s, j, s.length() - 1 - (i - j))){
+                    return s.length()- i;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private static boolean isValid(String s, int start, int end){
+        if(start >= end)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = start; i <= end; ++i){
+            char c = s.charAt(i);
+            switch (c){
                 case '(':{
-                    stk.push('(');
-                    ++cnt;
+                    stack.push('(');
                 } break;
                 case ')':{
-                    Character top = stk.pop();
-                    if(top.compareTo('(') != 0){
-                        stk.clear();
-                        cnt = 0;
+                    if(stack.empty() || stack.peek().compareTo('(') != 0){
+                        return false;
                     } else{
-                        if(ret < cnt){
-                            ret = cnt;
-                        }
-                        ++cnt;
+                        stack.pop();
                     }
                 } break;
             }
         }
-        return ret;
+
+        return stack.empty();
+    }
+
+    public static void main(String[] args){
+        var ret = new LongestVaildParentheses().longestValidParentheses(")()())");
+        System.out.println(ret);
     }
 }
