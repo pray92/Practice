d4f2a0282418f0d522bc3e208824cf43dbbb2421
jisu Choi <pray92@users.noreply.github.com>
2021-12-10 22:00:34 +0900

    Leet Code - FIX : Longest Vaild Parentheses

--- Java/src/LeetCode/LongestVaildParentheses.java
+++ Java/src/LeetCode/LongestVaildParentheses.java
@@ -14,10 +14,14 @@ public class LongestVaildParentheses {
             } else{
                 right++;
             }
-            if(left == right)
+            if(left == right){
                 ret = Math.max(ret, left + right);
+            } else if(right >= left){
+                left = right = 0;
+            }
         }
         // Right
+        left = 0; right = 0;
         for(int i = s.length() - 1; i >= 0; --i){
             char c = s.charAt(i);
             if(c == '('){
@@ -25,8 +29,11 @@ public class LongestVaildParentheses {
             } else{
                 right++;
             }
-            if(left == right)
+            if(left == right){
                 ret = Math.max(ret, left + right);
+            } else if(left >= right){
+                left = right = 0;
+            }
         }
 
         return ret;
