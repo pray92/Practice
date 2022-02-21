8373fe9a45235dfe951c8b340696fb8668433aae
Jisu Choi <pray92@users.noreply.github.com>
2022-02-21 23:21:57 +0900

    잘못된거 수정

--- Java/src/Programmers/QuadEncryption.java
+++ Java/src/Programmers/QuadEncryption.java
@@ -2,6 +2,7 @@ package Programmers;
 
 // 22:07 ~ 11:20
 class QuadEncription {
+
     // 0,1로 이뤄진 2^n * 2^n 2차원 배열, 쿼드 트리 형식으로 압축
     // S 공간에 숫자가 모두 같으면 해당 수 하나로 압축
     // 그렇지 않으면 S를 균일한 4개의 균일한 정사각형 영역으로 쪼개고 압축 시도
@@ -11,26 +12,7 @@ class QuadEncription {
         if(ret == 1){
             return new int[]{0, 1};
         } else if(ret == 0){
-            return new int[
-            private int[] getResult(int[][] arr, int startY, int endY, int startX, int endX){
-                if(startY > endY || startX > endX){
-                    return new int[]{0 ,0};
-                }
-
-                int ret = getEncriptResult(arr, startY, endY, startX, endX);
-                if(ret == 1){
-                    return new int[]{0, 1};
-                } else if(ret == 0){
-                    return new int[]{1, 0};
-                }
-
-                int[] next = new int[2];
-                next = getResult(arr, startY, (endY + startY) / 2, startX, (endX + startX) / 2);
-                next = addArray(next, getResult(arr, startY, (endY + startY) / 2, (startX + endX) / 2, endX));
-                next = addArray(next, getResult(arr, (endY + startY) / 2, endY, startX, (startX + endX) / 2));
-                next = addArray(next, getResult(arr, (endY + startY) / 2, endY, (startX + endX) / 2, endX));
-                return next;
-            }]{1, 0};
+            return new int[]{1, 0};
         }
         int[] answer = new int[2];
         int startY = 0, endY = arr.length;
@@ -44,6 +26,26 @@ class QuadEncription {
     }
 
 
+    private int[] getResult(int[][] arr, int startY, int endY, int startX, int endX){
+        if(startY > endY || startX > endX){
+            return new int[]{0 ,0};
+        }
+
+        int ret = getEncriptResult(arr, startY, endY, startX, endX);
+        if(ret == 1){
+            return new int[]{0, 1};
+        } else if(ret == 0){
+            return new int[]{1, 0};
+        }
+
+        int[] next = new int[2];
+        next = getResult(arr, startY, (endY + startY) / 2, startX, (endX + startX) / 2);
+        next = addArray(next, getResult(arr, startY, (endY + startY) / 2, (startX + endX) / 2, endX));
+        next = addArray(next, getResult(arr, (endY + startY) / 2, endY, startX, (startX + endX) / 2));
+        next = addArray(next, getResult(arr, (endY + startY) / 2, endY, (startX + endX) / 2, endX));
+        return next;
+    }
+
     private int[] addArray(int[] arr1, int[] arr2){
         int[] ret = new int[2];
         ret[0] = arr1[0] + arr2[0];
@@ -52,7 +54,7 @@ class QuadEncription {
     }
 
     private int getEncriptResult(int[][]arr, int startY, int endY, int startX, int endX){
-        if(startY > endY || startX > endX){
+        if(startY >= endY || startX >= endX){
             return -1;
         }
         int ret = arr[startY][startX];
